492d680d5f7dc237c813fdbaf9d2d56c0822d4d6
Karsten Schmidt <k@postspectacular.com>
2015-11-21 17:24:39 +0000

    disable obsolete HAL driver files in all build profiles, move led & clockconfig into /src/common, bugfix ex03/main.h imports

--- include/ex03/main.h
+++ include/ex03/main.h
@@ -5,6 +5,7 @@
 #include "stm32f401_discovery_accelerometer.h"
 #include "stm32f401_discovery_audio.h"
 #include "stm32f401_discovery_gyroscope.h"
+#include "stm32f4xx_it.h"
 #include "clockconfig.h"
 #include "led.h"
 #include "ex03/mems.h"
