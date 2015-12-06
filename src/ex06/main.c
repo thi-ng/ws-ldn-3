036a3c1211f06aa476137edd7191f75cb720e123
Karsten Schmidt <k@postspectacular.com>
2015-12-06 01:46:37 +0000

    update ex06

--- src/ex06/main.c
+++ src/ex06/main.c
@@ -12,9 +12,9 @@ static tinymt32_t rng;
 static __IO uint32_t transposeID = 0;
 
 static Synth synth;
-static SeqTrack* tracks[1];
+static SeqTrack* tracks[2];
 
-static uint8_t keyChanges[] = { 0, 5, 7, 9, 12, 19, 24 };
+static uint8_t keyChanges[] = { 0, 5, 7, 9 };
 
 static void playNote(Synth* synth, SeqTrack *track, int8_t note, uint32_t tick);
 static void updateAudioBuffer(Synth *synth);
@@ -30,7 +30,7 @@ int main(void) {
 		Error_Handler();
 	}
 
-	tinymt32_init(&rng, 0xdecafbad);
+	tinymt32_init(&rng, 0x12345678);
 
 	synth_init(&synth);
 	synth_bus_init(&(synth.bus[0]),
@@ -39,29 +39,39 @@ int main(void) {
 	synth_osc_init(&(synth.lfoEnvMod), synth_osc_sin_dc, 0, 0, 0, 1.0f);
 	BSP_AUDIO_OUT_Play((uint16_t*) &audioBuffer[0], AUDIO_BUFFER_SIZE);
 
-	int8_t notes[16] = { 36, -1, 12, 12, -1, -1, -1, -1, 48, -1, 17, 12, -1, -1, -1, 24 };
+	int8_t notes[] = { 36, -1, 12, 12, -1, -1, -1, -1, 48, -1, 17, 12, -1, -1,
+			-1, 24 };
+	int8_t notes2[] = { 12, 17, -1, 24, 36, -1 };
 
-	tracks[0] = initTrack((SeqTrack*) malloc(sizeof(SeqTrack)), playNote, notes, 16, 250, 1.0f);
+	tracks[0] = initTrack((SeqTrack*) malloc(sizeof(SeqTrack)), playNote, notes,
+			16, 250, 1.0f);
+	tracks[1] = initTrack((SeqTrack*) malloc(sizeof(SeqTrack)), playNote,
+			notes2, 6, 250, 1.0f);
 
 	while (1) {
 		uint32_t tick = HAL_GetTick();
-		updateAllTracks(&synth, tracks, 1, tick);
+		updateAllTracks(&synth, tracks, 2, tick);
 		updateAudioBuffer(&synth);
 	}
 }
 
 void playNote(Synth* synth, SeqTrack *track, int8_t note, uint32_t tick) {
 	float freq = notes[note - 12 + keyChanges[transposeID]];
-	uint32_t instID = 2;
 	//uint32_t instID = tinymt32_generate_uint32(&rng) & 3;
 	SynthVoice *voice = synth_new_voice(synth);
-	synth_adsr_init(&(voice->env), 0.025f, 0.000025f, 0.00005f, 1.0f, 0.95f);
-	synth_osc_init(&(voice->lfoPitch), synth_osc_sin, FREQ_TO_RAD(5.0f), 0.0f,
-			10.0f, 0.0f);
-	synth_osc_init(&(voice->osc[0]), instruments[instID], 0.15f, 0.0f, freq,
+	synth_adsr_init(&(voice->env), tinymt32_generate_float01(&rng) * 0.025f,
+			0.00015f, 0.00005f, 1.0f, 0.5f);
+	synth_osc_init(&(voice->lfoPitch), synth_osc_sin, FREQ_TO_RAD(2.0f), 0.0f,
+			1.0f, 0.0f);
+	synth_osc_init(&(voice->lfoMorph), synth_osc_nop, 0.0f, 0.0f, 0.0f, 0.5f + 0.5f * sinf(PI * 1.5f + tick * 0.00015f));
+//	synth_osc_init(&(voice->lfoMorph), synth_osc_sin_dc, 0.499f, 0.0f, 3520.0f + 3520.0f * sinf(PI * 1.5f + tick * 0.000075f), 0.5f);
+	synth_osc_init(&(voice->osc[0]), synth_osc_wtable_morph, 0.15f, 0.0f, freq,
 			0.0f);
-	synth_osc_init(&(voice->osc[1]), instruments[instID], 0.15f, 0.0f, freq * 1.01f,
-			0.0f);
+	synth_osc_set_wavetables(&(voice->osc[0]), wtable_sin_exp2, wtable_sin_pow2);
+	synth_osc_init(&(voice->osc[1]), synth_osc_wtable_morph, 0.15f, 0.0f,
+			freq * 0.501f, 0.0f);
+	synth_osc_set_wavetables(&(voice->osc[1]), wtable_sin_exp2,
+			wtable_super_saw);
 	BSP_LED_Toggle(LED_GREEN);
 }
 
@@ -83,7 +93,9 @@ void HAL_GPIO_EXTI_Callback(uint16_t pin) {
 	if (pin == KEY_BUTTON_PIN) {
 		if (!isPressed) {
 			BSP_LED_Toggle(LED_BLUE);
-			transposeID = (transposeID + 1) % 7;
+			transposeID = (transposeID + 1) % 4;
+			tracks[0]->direction *= -1;
+			tracks[1]->direction *= -1;
 			isPressed = 1;
 		} else {
 			isPressed = 0;
