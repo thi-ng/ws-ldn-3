7c77e9c7381f9fe07bb99798780ac5e789d3ea2a
Karsten Schmidt <k@postspectacular.com>
2015-12-07 17:58:46 +0000

    minor update wavetable lookup macros

--- include/synth/wavetable.h
+++ include/synth/wavetable.h
@@ -5,10 +5,10 @@
 #include "synth/synth.h"
 
 #define WAVE_TABLE_LENGTH		1024
-#define WAVE_TABLE_SCALE		(WAVE_TABLE_LENGTH / TAU)
+#define WAVE_TABLE_SCALE		((float)WAVE_TABLE_LENGTH * INV_TAU)
 #define WTABLE_INDEX(x)         ((uint32_t)((x) * WAVE_TABLE_SCALE))
-#define WTABLE_LOOKUP(table, x)	(*(table + WTABLE_INDEX(x)))
-#define WTABLE_LOOKUP_RAW(table, idx) (*(table + (idx)))
+#define WTABLE_LOOKUP(table, x)	(table[WTABLE_INDEX(x)])
+#define WTABLE_LOOKUP_RAW(table, idx) (table[idx])
 
 extern const float wtable_sin[WAVE_TABLE_LENGTH];
 extern const float wtable_harmonics_1[WAVE_TABLE_LENGTH];
